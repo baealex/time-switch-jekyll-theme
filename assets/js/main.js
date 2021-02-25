9f622323f52de361fa606cde05dee0104986a5c3
baealex <im@baejino.com>
2021-02-26 04:16:39 +0900

    ⭐ Add utterances

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,53 @@
 ---
 ---
+
+var hasUtterances = false;
+
 function getCookie(name) {
     var value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
     return value? value[2] : null;   
 }
 
-function NightMode() {
+function setNightMode() {
+    var setUtterancesTheme = function(theme) {
+        const message = {
+            type: 'set-theme',
+            theme: theme
+        };
+        var utterances = document.querySelector('iframe');
+        utterances.contentWindow.postMessage(message, 'https://utteranc.es');
+    };
+
     if(getCookie('nightmode')) {
         document.getElementById('nightToggle').checked = true;
-        $('.target').addClass('nightmode');
+        $('body').addClass('dark');
+        if(hasUtterances) {
+            setUtterancesTheme('github-dark');
+        }
     } else {
-        $('.target').removeClass('nightmode');
+        $('body').removeClass('dark');
+        if(hasUtterances) {
+            setUtterancesTheme('github-light');
+        }
     }
 }
 
-function SetNightMode() {
+addEventListener('message', event => {
+    if (event.origin !== 'https://utteranc.es') {
+        return;
+    }
+    hasUtterances = true;
+    setNightMode();
+});
+
+$('#nightToggle').on('click', function() {
     if(!getCookie('nightmode')) {
-        var date=new Date();
-        document.cookie='nightmode=true; expries='+ (date.getDate()+7) +'; path={{ site.baseurl }}';
+        var date = new Date();
+        document.cookie='nightmode=true; expries='+ (date.getDate()+30) +'; path={{ site.baseurl }}';
     } else {
         document.cookie = 'nightmode=; expires=Thu, 18 Dec 2013 12:00:00 UTC; path={{ site.baseurl }}';
     }
-    NightMode();
-}
-NightMode();
+    setNightMode();
+});
+
+setNightMode();
